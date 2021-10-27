ae26ce53d532211f15d41f8c515b2ea6567a900a
Juan Cuenca <juan99.cuenca@gmail.com>
2021-10-27 16:17:22 -0300

    Finished create post funtionability

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render, redirect
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        if title == "" or body == "":
+            return render(request, 'index.html', {'posts':Post.objects.all(), 'message': 'Empty field/s. Post not added'})
+
+        post = Post.objects.create(title= title, body=body)
+        post.save()
+
+
+    
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts':posts})
-
 
 def post(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, "post.html", {"post":post})
+
